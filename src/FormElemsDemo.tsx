315419184ef559107e3fd7ac9146f61538afc8ff
gaoyan <magaoyanxj@gmail.com>
2024-04-28 17:51:06 +0800

    chore: 使用checkbox复选按钮

--- src/FormElemsDemo.tsx
+++ src/FormElemsDemo.tsx
@@ -1,34 +1,56 @@
-import React, { FC, useState, ChangeEvent } from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
 
 const FormElemsDemo: FC = () => {
-  const [gender, setGender] = useState('male')
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setGender(event.target.value)
+  // const [checked, setChecked] = useState(false)
+  // function toggleChecked() {
+  //   setChecked(!checked)
+  // }
+  const [selectedCityList, setSelectedCityList] = useState<string[]>([])
+
+  function handleCityChange(event: ChangeEvent<HTMLInputElement>) {
+    const city = event.target.value
+    if (selectedCityList.includes(city)) {
+      setSelectedCityList(
+        selectedCityList.filter(c => {
+          if (c === city) return false
+          return true
+        })
+      )
+    } else {
+      setSelectedCityList(selectedCityList.concat(city))
+    }
   }
   return (
     <>
       <p>form elems demo</p>
-      <div>
-        <label htmlFor="radio1">男</label>
-        <input
-          type="radio"
-          id="radio1"
-          name="gender"
-          value="male"
-          checked={gender === 'male'}
-          onChange={handleChange}
-        />
-        <label htmlFor="radio2">女</label>
-        <input
-          type="radio"
-          id="radio2"
-          name="gender"
-          value="female"
-          checked={gender === 'female'}
-          onChange={handleChange}
-        />
-        <button onClick={() => console.log(gender)}>print</button>
-      </div>
+      {/* <label htmlFor="checkbox1">选中</label>
+      <input type="checkbox" id="checkbox1" onChange={toggleChecked} checked={checked} /> */}
+      <label htmlFor="checkbox1">北京</label>
+      <input
+        type="checkbox"
+        id="checkbox1"
+        value="beijing"
+        checked={selectedCityList.includes('beijing')}
+        onChange={handleCityChange}
+      />
+      <label htmlFor="checkbox2">上海</label>
+      <input
+        type="checkbox"
+        id="checkbox2"
+        value="shanghai"
+        checked={selectedCityList.includes('shanghai')}
+        onChange={handleCityChange}
+      />
+      <label htmlFor="checkbox3">深圳</label>
+      <input
+        type="checkbox"
+        id="checkbox3"
+        value="shenzhen"
+        checked={selectedCityList.includes('shenzhen')}
+        onChange={handleCityChange}
+      />
+      {JSON.stringify(selectedCityList)}
+      <input type="hidden" name="cities" value={JSON.stringify(selectedCityList)} />
     </>
   )
 }
