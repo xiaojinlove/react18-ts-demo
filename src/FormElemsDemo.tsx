1300426ba50dfdc57edf6aed37e4facab0e1304a
gaoyan <magaoyanxj@gmail.com>
2024-04-28 18:06:22 +0800

    chore: 使用select下拉框组件

--- src/FormElemsDemo.tsx
+++ src/FormElemsDemo.tsx
@@ -1,56 +1,20 @@
 import React, { ChangeEvent, FC, useState } from 'react'
 
 const FormElemsDemo: FC = () => {
-  // const [checked, setChecked] = useState(false)
-  // function toggleChecked() {
-  //   setChecked(!checked)
-  // }
-  const [selectedCityList, setSelectedCityList] = useState<string[]>([])
-
-  function handleCityChange(event: ChangeEvent<HTMLInputElement>) {
-    const city = event.target.value
-    if (selectedCityList.includes(city)) {
-      setSelectedCityList(
-        selectedCityList.filter(c => {
-          if (c === city) return false
-          return true
-        })
-      )
-    } else {
-      setSelectedCityList(selectedCityList.concat(city))
-    }
+  const [lang, setLang] = useState('js')
+  function handleChange(event: ChangeEvent<HTMLSelectElement>) {
+    setLang(event.target.value)
   }
   return (
     <>
       <p>form elems demo</p>
-      {/* <label htmlFor="checkbox1">选中</label>
-      <input type="checkbox" id="checkbox1" onChange={toggleChecked} checked={checked} /> */}
-      <label htmlFor="checkbox1">北京</label>
-      <input
-        type="checkbox"
-        id="checkbox1"
-        value="beijing"
-        checked={selectedCityList.includes('beijing')}
-        onChange={handleCityChange}
-      />
-      <label htmlFor="checkbox2">上海</label>
-      <input
-        type="checkbox"
-        id="checkbox2"
-        value="shanghai"
-        checked={selectedCityList.includes('shanghai')}
-        onChange={handleCityChange}
-      />
-      <label htmlFor="checkbox3">深圳</label>
-      <input
-        type="checkbox"
-        id="checkbox3"
-        value="shenzhen"
-        checked={selectedCityList.includes('shenzhen')}
-        onChange={handleCityChange}
-      />
-      {JSON.stringify(selectedCityList)}
-      <input type="hidden" name="cities" value={JSON.stringify(selectedCityList)} />
+      <div>
+        <select value={lang} onChange={handleChange}>
+          <option value="java">Java</option>
+          <option value="js">JS</option>
+          <option value="css">CSS</option>
+        </select>
+      </div>
     </>
   )
 }
