b854e70dc1ed00f557453d76ee46751a23bcc6c4
gaoyan <magaoyanxj@gmail.com>
2024-04-28 15:45:58 +0800

    chore: 受控组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 //import ClosureDemo from './ClosureTrap'
 //import useMouse from './hooks/useMouse'
 //import useGetInfo from './hooks/useGetInfo'
 //import List1 from './List1'
 //import List2 from './List2'
-import StyledComponentDemo from './StyledComponentDemo'
+//import StyledComponentDemo from './StyledComponentDemo'
 //import UseRefDemo from './UseRefDemo'
 //import ImmerDemo from './ImmerDemo'
 //import UseMemoDemo from './UseMemoDemo'
@@ -16,10 +16,19 @@ function App() {
   //useTitle('App page 1')
   // const { x, y } = useMouse()
   //const { loading, info } = useGetInfo()
+  const [text, setText] = useState<string>('hello')
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value)
+  }
   return (
     <>
-      {/* <p>App page</p>
-      <ClosureDemo /> */}
+      <p>App page</p>
+      <div>
+        <input value={text} onChange={handleChange} />
+        <button onClick={() => console.log(text)}>打印</button>
+        <button onClick={() => setText('jam')}>set</button>
+      </div>
+      {/* <ClosureDemo /> */}
       {/* <p>{loading ? '加载中...' : info}</p> */}
       {/* <List1 /> */}
       {/* <StateDemo1 /> */}
@@ -29,7 +38,7 @@ function App() {
       {/* <UseRefDemo /> */}
       {/* <UseMemoDemo /> */}
       {/* <UseCallbackDemo /> */}
-      <StyledComponentDemo />
+      {/* <StyledComponentDemo /> */}
     </>
   )
 }
