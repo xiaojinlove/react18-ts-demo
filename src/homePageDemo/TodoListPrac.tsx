c5ecd302d3f6783b3ad113a03df50e0b8b3bee5b
gaoyan <magaoyanxj@gmail.com>
2024-07-29 11:27:34 +0800

    chore: todoList练习第一步完善

--- src/homePageDemo/TodoListPrac.tsx
+++ src/homePageDemo/TodoListPrac.tsx
@@ -1,19 +1,25 @@
 import React, { ChangeEvent, FC, useState } from 'react'
 import { nanoid } from 'nanoid'
 
+type TodoType = {
+  id: string
+  title: string
+}
 const TodoListPrac: FC = () => {
   const initialState = [
     { id: nanoid(5), title: '吃饭' },
     { id: nanoid(5), title: '睡觉' },
   ]
-  const [list, setList] = useState(initialState)
+  const [list, setList] = useState<TodoType[]>(initialState)
   const [text, setText] = useState('')
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     setText(event.target.value)
   }
   function handleSubmit(event: ChangeEvent<HTMLFormElement>) {
     event.preventDefault()
+    if (!text.trim()) return
     setList(list.concat({ id: nanoid(5), title: text }))
+    setText('')
   }
   return (
     <div>
@@ -25,7 +31,7 @@ const TodoListPrac: FC = () => {
       <p>What needs to be done?</p>
       <form onSubmit={handleSubmit}>
         <input value={text} onChange={handleChange} />
-        <button type="submit">添加todo</button>
+        <button type="submit">Add #{list.length + 1}</button>
       </form>
     </div>
   )
