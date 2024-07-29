1c122964483cb162903c16d9fad685c5c36cb9b8
gaoyan <magaoyanxj@gmail.com>
2024-07-29 11:56:51 +0800

    chore: todoList练习第二步拆饭组件

--- src/homePageDemo/TodoListPrac.tsx
+++ src/homePageDemo/TodoListPrac.tsx
@@ -11,6 +11,33 @@ const TodoListPrac: FC = () => {
     { id: nanoid(5), title: '睡觉' },
   ]
   const [list, setList] = useState<TodoType[]>(initialState)
+
+  return (
+    <div>
+      <List list={list} />
+      <InputForm list={list} setList={setList} />
+    </div>
+  )
+}
+type ListPropsType = {
+  list: TodoType[]
+}
+const List: FC<ListPropsType> = props => {
+  const { list } = props
+  return (
+    <ul>
+      {list.map(item => {
+        return <li key={item.id}>{item.title}</li>
+      })}
+    </ul>
+  )
+}
+type InputFormPropsType = {
+  list: TodoType[]
+  setList: (list: TodoType[]) => void
+}
+const InputForm: FC<InputFormPropsType> = props => {
+  const { list, setList } = props
   const [text, setText] = useState('')
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     setText(event.target.value)
@@ -22,19 +49,11 @@ const TodoListPrac: FC = () => {
     setText('')
   }
   return (
-    <div>
-      <ul>
-        {list.map(item => {
-          return <li key={item.id}>{item.title}</li>
-        })}
-      </ul>
+    <form onSubmit={handleSubmit}>
       <p>What needs to be done?</p>
-      <form onSubmit={handleSubmit}>
-        <input value={text} onChange={handleChange} />
-        <button type="submit">Add #{list.length + 1}</button>
-      </form>
-    </div>
+      <input value={text} onChange={handleChange} />
+      <button type="submit">Add #{list.length + 1}</button>
+    </form>
   )
 }
-
 export default TodoListPrac
